3fa9062dc140c4c3fe0d66546e39576c7e513cbe
Althaf Nawshad <althafnawshad37@gmail.com>
2022-07-20 01:50:06 +0300

    added mongodb dependancy and required migrations

---
+++ database/migrations/2022_07_19_154936_create_daily_steps_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mongodb')->create('daily_steps', function ($collection) {
+            $collection->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('mongodb')->dropIfExists('daily_steps');
+    }
+};
